5431f3d11d3bf6a0509537fee19914719142404c
NeZox_off <mpolakov777@gmail.com>
2025-03-25 09:19:17 +0200

    styled home page

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -2,17 +2,37 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { Button, Checkbox, Icon, Input, Label } from "../components/ui";
-
+import { useForm, SubmitHandle } from "react-hook-form";
 interface AuthProps {}
+
+type Inputs = {
+  email: string;
+  password: string;
+  rememberMe?: boolean;
+};
 
 const Auth = ({}: AuthProps) => {
   const [isHiddenPass, setIsHiddenPass] = useState(true);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const handleSwitchHiddenPass = () => {
+    setIsHiddenPass((prev) => !prev);
+  };
+
+  const onSubmit: SubmitHandle<Inputs> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <section className="py-14">
-      <div className="container px-16 mx-auto w-full">
-        <div className="flex items-center">
+    <section className="py-14 h-dvh">
+      <div className="container px-16 mx-auto w-full h-full">
+        <div className="flex items-center justify-between h-full">
           <div>
-            <div className="max-w-[34.375rem]">
+            <div className="max-w-[34.375rem] mb-60">
               <h1 className="text-7xl max-w-[29.188rem]">
                 Nexery - control panel
               </h1>
@@ -27,48 +47,66 @@ const Auth = ({}: AuthProps) => {
               height={162}
             />
           </div>
-          <form>
+          <form
+            className="max-w-2xl w-full h-full p-16 rounded-3xl bg-card flex flex-col justify-center"
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <div>
-              <div>
-                <div>
-                  <h2>Log in</h2>
-                  <p>Welcome back!</p>
+              <div className="mb-24">
+                <div className="text-center mb-8">
+                  <h2 className="text-5xl">Log in</h2>
+                  <p className="text-2xl font-medium text-white/40">
+                    Welcome back!
+                  </p>
                 </div>
-                <div>
-                  <Label>
-                    Email
-                    <Input />
-                  </Label>
-                  <Label>
-                    <div>
-                      Password
-                      <span>
+                <div className="flex flex-col gap-3">
+                  <div>
+                    <Label htmlFor="email" className="mb-1 text-white/20">Email</Label>
+                    <Input
+                      id="email"
+                      type="text"
+                      className="px-5 py-4"
+                      defaultValue={"example@gmail.com"}
+                      {...register("email")}
+                    />
+                  </div>
+                  <div>
+                    <div className="flex items-center justify-between mb-1">
+                      <Label htmlFor="password" className="text-white/20">Password</Label>
+                      <span
+                        onClick={() => handleSwitchHiddenPass()}
+                        className="flex items-center gap-3 text-white/20"
+                      >
                         {isHiddenPass ? (
-                          <>
-                            <Icon name="Eye" />
-                            Hide
-                          </>
-                        ) : (
                           <>
                             <Icon name="EyeClosed" />
                             Show
                           </>
+                        ) : (
+                          <>
+                            <Icon name="Eye" />
+                            Hide
+                          </>
                         )}
                       </span>
                     </div>
-                    {isHiddenPass ? <Input /> : <Input />}
-                  </Label>
+                    {isHiddenPass ? (
+                      <Input id="password" type="password" className="px-5 py-4" placeholder="example" />
+                    ) : (
+                      <Input id="password" type="text" className="px-5 py-4" placeholder="example" />
+                    )}
+                  </div>
                   <div className="flex items-center space-x-2">
                     <Checkbox id="remember" />
-                    <Label htmlFor="remember">
-                      Accept terms and conditions
+                    <Label htmlFor="remember" className="text-white/40">
+                      Remember me
                     </Label>
                   </div>
                 </div>
               </div>
-              <div>
-                <Button>Login</Button>
-                <span>Forgot your password?</span>
+              <div className="flex flex-col items-center justify-center gap-8">
+                <Button className="w-full font-bold">Login</Button>
+                <span className="text-primary">Forgot your password?</span>
               </div>
             </div>
           </form>
